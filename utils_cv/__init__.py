f3e22d1bb08c87833b2e3ce67c481491540db39d
Gregoire Martin <gregoire.martin@student-cs.fr>
2018-11-13 16:40:46 +0100

    rajout d'une fonction de rotation image dans init.py

--- utils_cv/__init__.py
+++ utils_cv/__init__.py
@@ -5,3 +5,11 @@ def load_and_display_image(filename):
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def process_image(filename):
+    img=cv2.imread(filename,cv2.IMREAD_COLOR)
+    longueur,largeur=img.shape()
+    matricederotation=cv2.getRotationMatrix2D((largeur/2,longueur/2),90,1)
+    dst=cv2.warpAffine(img,matricederotation,(largeur,longueur))
+    cv2.imshow('image',dst)
