8785d39cfd976d6853151ed7e12944edb9ebe010
2018puiga <axel.puig@student-cs.fr>
2018-11-14 15:39:54 +0100

    Resizing image because it was too big

--- facerecognition/__init__.py
+++ facerecognition/__init__.py
@@ -6,6 +6,7 @@ def detection_visage_yeux(filename):
     face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
     img = cv2.imread(filename)
+    img = cv2.resize(img, (500, 500))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
