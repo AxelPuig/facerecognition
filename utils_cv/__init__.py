af47f29ba7fb9627446ec552b5eed3d13cee4a61
Gregoire Martin <gregoire.martin@student-cs.fr>
2018-11-14 11:11:20 +0100

    rajout d'une fonction translation

--- utils_cv/__init__.py
+++ utils_cv/__init__.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def load_and_display_image(filename):
@@ -15,5 +16,15 @@ def process_image(filename):
     matricederotation = cv2.getRotationMatrix2D((longueur, largeur / 2), 90, 1)
     dst = cv2.warpAffine(img, matricederotation, (longueur, largeur))
     cv2.imshow('image', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def translation_image(filename):
+    img = cv2.imread(filename, cv2.IMREAD_COLOR)
+    hauteur, longueur, _ = img.shape
+    M = np.float32([[1, 0, 100], [0, 1, 50]])
+    dst = cv2.warpAffine(img, M, (longueur + 100, hauteur + 50))
+    cv2.imshow('img', dst)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
