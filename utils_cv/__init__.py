e870e391587758e3c0675612e95d7d197afaa59c
2018puiga <axel.puig@student-cs.fr>
2018-11-14 09:56:40 +0100

    Correcting process_image bug

--- utils_cv/__init__.py
+++ utils_cv/__init__.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 def load_and_display_image(filename):
     img = cv2.imread(filename, cv2.IMREAD_COLOR)
     cv2.imshow('image', img)
@@ -8,8 +9,11 @@ def load_and_display_image(filename):
 
 
 def process_image(filename):
-    img=cv2.imread(filename,cv2.IMREAD_COLOR)
-    longueur,largeur=img.shape()
-    matricederotation=cv2.getRotationMatrix2D((largeur/2,longueur/2),90,1)
-    dst=cv2.warpAffine(img,matricederotation,(largeur,longueur))
-    cv2.imshow('image',dst)
+    img = cv2.imread(filename, cv2.IMREAD_COLOR)
+    print(img.shape)
+    longueur, largeur, _ = img.shape
+    matricederotation = cv2.getRotationMatrix2D((longueur, largeur / 2), 90, 1)
+    dst = cv2.warpAffine(img, matricederotation, (longueur, largeur))
+    cv2.imshow('image', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
