a69ea041b45f9856e7ece7ad9ad37a568d85c8f8
2018puiga <axel.puig@student-cs.fr>
2018-11-15 09:37:54 +0100

    Face detection and encoding

--- facerecognition/recognize_faces_image.py
+++ facerecognition/recognize_faces_image.py
@@ -13,4 +13,22 @@ ap.add_argument("-i", "--image", required=True,
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
                 help="face detection model to use: either `hog` or `cnn`")
 args = vars(ap.parse_args())
+
+# load the known faces and embeddings
+print("[INFO] loading encodings...")
+data = pickle.loads(open(args["encodings"], "rb").read())
+
+# load the input image and convert it from BGR to RGB
+image = cv2.imread(args["image"])
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# detect the (x, y)-coordinates of the bounding boxes corresponding
+# to each face in the input image, then compute the facial embeddings
+# for each face
+print("[INFO] recognizing faces...")
+boxes = face_recognition.face_locations(rgb,
+                                        model=args["detection_method"])
+encodings = face_recognition.face_encodings(rgb, boxes)
+
+# initialize the list of names for each face detected
 names = []
